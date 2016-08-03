7d821d611d10ca09c4587f88cdd967419937c57e
Russell Schmidt <russell.schmidt@me.com>
2016-08-02 23:16:44 -0700

    add queue to myqueue.rb

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,18 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue << element
+    @tail = @queue[-1]
   end
 
   def dequeue
+    if !@queue.empty?
+      @queue.shift
+      @tail = @queue[-1]
+      @head = @queue[0]
   end
 
   def empty?
+    @queue.count == 0 ? true : false
   end
 end
