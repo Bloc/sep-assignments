083ffa485e4094f851232b546519f3da33ee80e9
Richard Newman <richard@newmanworks.com>
2016-08-06 15:15:46 -0600

    Assignment 1: pixel exercise

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,24 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    self.width = width
+    self.height = height
+    self.matrix = Array.new(width, Array.new(height, nil))
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    matrix[x][y] = pixel if inbounds(x, y)
   end
 
   def at(x, y)
+    inbounds(x, y) ? matrix[x][y] : nil
   end
 
   private
 
   def inbounds(x, y)
+    ((0..width).include?(x)) && ((0..height).include?(y))
   end
 
 end
