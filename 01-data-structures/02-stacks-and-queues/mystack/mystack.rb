1816fbeecc6b2bc4c26806f1a68b7aacaff873ee
Americool <aberanderson@gmail.com>
2016-08-10 18:11:12 -0700

    halfway-done with assignment2

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -4,14 +4,27 @@ class MyStack
   def initialize
     @stack = Array.new
     self.top = nil
+    @position = 0
   end
 
   def push(item)
+    @position += 1
+    @stack[@position] = item
+    self.top = item
   end
 
   def pop
+    temp = @stack[@position]
+    @position -=1
+    self.top = @stack[@position]
+    return temp
   end
 
   def empty?
+    if self.top == nil
+      true
+    else
+      false 
+    end
   end
 end
