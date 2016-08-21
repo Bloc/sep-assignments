de32720d88ca108e44e98785714c1f0e448323cd
Russell Schmidt <russell.schmidt@me.com>
2016-08-20 18:12:16 -0700

    insert working on Binary Search Tree

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -2,14 +2,42 @@ require_relative 'node'
 
 class BinarySearchTree
 
+  attr_accessor :root
+
   def initialize(root)
+    self.root = root
   end
 
   def insert(root, node)
+    if node.rating > root.rating
+      if root.right == nil
+        root.right = node
+      else 
+        insert(root.right, node)
+      end
+    elsif node.rating < root.rating
+      if root.left == nil
+        root.left = node
+      else
+        insert(root.left, node)
+      end
+    end
   end
 
   # Recursive Depth First Search
   def find(root, data)
+    target = nil
+    if root.title == data.title
+      target = root
+    elsif data.rating < root.rating
+      find(root.left, data)
+
+    elsif data.rating > root.rating
+      find(root.right, data)
+    else
+      nil
+    end
+    return target
   end
 
   def delete(root, data)
