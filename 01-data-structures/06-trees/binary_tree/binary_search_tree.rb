156eae1ed9f9655cb2309a3d7d924b29a3fa6bde
Russell Schmidt <russell.schmidt@me.com>
2016-08-22 20:55:36 -0700

    add favorite movies and add recursive_breadth_find method

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -2,15 +2,45 @@ require_relative 'node'
 
 class BinarySearchTree
 
-  attr_accessor :root, :movie_list
+  attr_accessor :root, :movie_list, :favorite_movies
 
   def initialize(root)
     self.root = root
     self.movie_list = []
+
+    self.favorite_movies = [
+      Node.new("John Wick", 85),
+      Node.new("Dune (1984)", 57),
+      Node.new("Irreversible", 58),
+      Node.new("Skyfall", 93),
+      Node.new("Casino Royale", 95),
+      Node.new("Aliens", 98),
+      Node.new("The Godfather", 99),
+      Node.new("The Social Network", 96),
+      Node.new("Moon", 89),
+      Node.new("Ex Machina", 93),
+      Node.new("Event Horizon", 24),
+      Node.new("Starship Troopers", 63),
+      Node.new("Robocop (1987)", 88),
+      Node.new("Total Recall", 56),
+      Node.new("Fight Club", 80),
+      Node.new("Pulp Fiction", 91),
+      Node.new("Melancholia", 79),
+      Node.new("Nymphomaniac: Volume I", 75),
+      Node.new("Nymphomaniac: Volume II", 60),
+      Node.new("The Fifth Element", 71),
+      Node.new("Out For Justice", 19),
+      Node.new("Star Wars: The Empire Strikes Back", 94),
+      Node.new("Star Wars: A New Hope", 93),
+      Node.new("Valhalla Rising", 70),
+      Node.new("Hammer of the Gods", 30),
+    ]
   end
 
   def insert(root, node)
-    if node.rating > root.rating
+    if root.nil?
+      self.root = node
+    elsif node.rating > root.rating
       if root.right == nil
         root.right = node
       else 
@@ -86,6 +116,7 @@ class BinarySearchTree
     if nodes.nil?
       nodes = [self.root]
     end
+
     nodes.each do |node|
       if !node.nil?
         puts "#{node.title}: #{node.rating}"
@@ -98,4 +129,38 @@ class BinarySearchTree
       printf(next_row)
     end
   end
+
+  def create_movie_tree
+    insert(nil, self.favorite_movies[0])
+
+    self.favorite_movies.each do |movie|
+      if self.recursive_breadth_find(self.root, movie.title).nil?
+        self.insert(self.root, movie)
+      end
+    end
+  end
+
+  # node.title search
+  def recursive_breadth_find(nodes, data)
+    next_row = []
+
+    if nodes.nil?
+      nodes = [self.root]
+    end
+
+    nodes.each do |node|
+      if !node.nil?
+        if node.title == data
+          return node
+        else
+        next_row << node.left
+        next_row << node.right
+      end
+    end
+
+    if next_row.count > 0
+      recursive_breadth_find(next_row)
+    end
+  end
+
 end
