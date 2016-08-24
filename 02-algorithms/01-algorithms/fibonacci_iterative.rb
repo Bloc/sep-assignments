13c8dfa0db943d37f61c8f0abc1a5adc3be538be
Estefania Flores <eflores0227@gmail.com>
2016-08-24 12:19:05 -0700

    intro to algorithms solutions

---
+++ 02-algorithms/01-algorithms/fibonacci_iterative.rb
@@ -0,0 +1,21 @@
+def fib(n)
+   fib_0 = 0
+   fib_1 = 1
+   (0..n-1).each do |x|
+     temp = fib_0
+     fib_0 = fib_1
+     fib_1 = temp + fib_1
+   end
+   return fib_1
+ end
+
+ puts fib(0)
+ puts fib(1)
+ puts fib(2)
+ puts fib(3)
+ puts fib(4)
+ puts fib(5)
+ puts fib(6)
+ puts fib(7)
+ puts fib(8)
+ puts fib(9)
