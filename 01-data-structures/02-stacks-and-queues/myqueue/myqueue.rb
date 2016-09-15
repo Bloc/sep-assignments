b09f4e98e9d1be89a2054f34d338e3f125a06cfc
Jason Divosevic <jdivosevic@gmail.com>
2016-09-15 11:18:31 -0400

    Assignment 2 Completed

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,20 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[0]
   end
 
   def enqueue(element)
+    @queue.unshift(element)
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def dequeue
+    @queue.shift
   end
 
   def empty?
+    return @queue.length == 0
   end
 end
