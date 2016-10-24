eff6add8a3be4db90b4662400ae983ef9c9c82f6
Jason Divosevic <jdivosevic@gmail.com>
2016-10-23 23:03:07 -0400

    revised binary search recursive completed

--- 02-algorithms-complexity/02-algorithms-searching/binary_search_recursive.rb
+++ 02-algorithms-complexity/02-algorithms-searching/binary_search_recursive.rb
@@ -1,24 +1,28 @@
 def binary_search_recursive(collection, value)
-  low = 0
   high = collection.length - 1
+  low = 0
   mid = (high + low) / 2
 
-  if collection[mid] == value
+  if collection.empty?
+    return "value not found"
+  elsif collection[mid] == value
     return collection[mid]
   elsif collection[mid] < value
-    binary_search_recursive(collection.slice(0.. mid -1), value)
+    binary_search_recursive(collection.slice(mid + 1..high), value)
   elsif collection[mid] > value
-    binary_search_recursive(collection.slice(mid + 1.. high), value)
+    binary_search_recursive(collection.slice(0..mid - 1), value)
   end
-
-  return "value not found"
 end
 
 collection = [0,5,10,15,20,25,30]
 
-position = binary_search_recursive(collection, 0)
+position = binary_search_recursive(collection, 15)
 puts position
 position = binary_search_recursive(collection, 1)
 puts position
 position = binary_search_recursive(collection, 10)
+puts position
+position = binary_search_recursive(collection, 30)
+puts position
+position = binary_search_recursive(collection, 35)
 puts position
