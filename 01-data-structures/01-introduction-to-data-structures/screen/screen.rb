147c8d28d3ba6a0db90c3c1f1a9f615982d892b5
Allen Kramer <kramer.alleng@gmail.com>
2016-11-09 11:02:04 -0500

    edited intro-answers.txt to answer 3 questions for line.rb

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,9 +6,9 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
-    self.width = width
-    self.height = height
-    self.matrix = []
+    @width = width
+    @height = height
+    @matrix = []
   end
 
   # Insert a Pixel at x, y
