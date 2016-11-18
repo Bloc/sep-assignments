035a83fe536c8877ce8c072ece4c1d3ba44ff116
Allen Kramer <kramer.alleng@gmail.com>
2016-11-18 08:42:39 -0500

    finished code for hashclass, answered questions in .txt file

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,16 +5,50 @@ class HashClass
     @size = size
   end
 
+  # set
   def []=(key, value)
+    index = index(key, @size)
 
+    # empty hash
+    if @items[index].nil?
+      @items[index] = HashItem.new(key, value)
+
+    # new key
+    elsif @items[index].key != key
+      resize()
+      self[key] = value
+
+      # same key, new value
+    elsif @items[index].key === key && @items[index].value != value
+      resize()
+      @items[index(key, @size)].value = value
+
+    # else - do nothing if same key same value (no else statement)
+    end
+
+    p @items
+    p @size
   end
 
+  # get
   def [](key)
-
+    if @items[index(key, @size)].key === key
+      return @items[index(key, @size)].value
+    end
   end
 
   def resize
+    @size = 2 * @size
+    temp = Array.new(@size)
 
+    for entry in @items do
+      if entry
+        new_index = index(entry.key, @size)
+        temp[new_index] = entry
+      end
+    end
+
+    @items = temp
   end
 
   # Returns a unique, deterministically reproducible index into an array
