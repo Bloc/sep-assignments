de3ea4c503a865df538a4862dac4ff7bdfb83424
Devon Henegar <devon.henegar@gmail.com>
2016-12-06 11:22:00 -0500

    assignment two passing all tests

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,34 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[0]
+    @index = [nil,0]
+
   end
 
   def enqueue(element)
+    @queue[@index[1]] = element
+    @head = element
+    if @index[0] == nil
+      @index[0] = 0
+      @tail = @queue[0]
+    end
+    @index[1] += 1
   end
 
   def dequeue
+    item = @tail
+    @queue.shift
+    @tail = @queue[0]
+    @index[1] -= 1
+    if empty?
+      @head = nil
+      @tail = nil
+    end
+    return item
   end
 
   def empty?
+    return @index[1] == 0
   end
 end
