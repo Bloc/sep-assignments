a0f02365754e9ef00bf25ea0d379593c4bea248c
Allen Kramer <kramer.alleng@gmail.com>
2016-12-07 08:39:08 -0500

    revised test in separate_chaining.rb

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -88,17 +88,15 @@ class SeparateChaining
 end
 
 
-def test_function
-  # require "./separate_chaining/separate_chaining.rb"
-
-  star_wars_movies = SeparateChaining.new(6)
-  star_wars_movies["Star Wars: The Phantom Menace"] = "Number One"
-  star_wars_movies["Star Wars: Attack of the Clones"] = "Number Two"
-  star_wars_movies["Star Wars: Revenge of the Sith"] = "Number Three"
-  star_wars_movies["Star Wars: A New Hope"] = "Number Four"
-  star_wars_movies["Star Wars: The Empire Strikes Back"] = "Number Five"
-  star_wars_movies["Star Wars: Return of the Jedi"] = "Number Six"
-  star_wars_movies.print()
-end
-
-# test_function()
+# def test_function
+#   require "./separate_chaining/separate_chaining.rb"
+#
+#   star_wars_movies = SeparateChaining.new(6)
+#   star_wars_movies["Star Wars: The Phantom Menace"] = "Number One"
+#   star_wars_movies["Star Wars: Attack of the Clones"] = "Number Two"
+#   star_wars_movies["Star Wars: Revenge of the Sith"] = "Number Three"
+#   star_wars_movies["Star Wars: A New Hope"] = "Number Four"
+#   star_wars_movies["Star Wars: The Empire Strikes Back"] = "Number Five"
+#   star_wars_movies["Star Wars: Return of the Jedi"] = "Number Six"
+#   star_wars_movies.print()
+# end
