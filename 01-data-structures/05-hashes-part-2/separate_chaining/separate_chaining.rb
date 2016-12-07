59cc0eb01e08499b8254c2c731a665646b77d5f6
Allen Kramer <kramer.alleng@gmail.com>
2016-12-07 07:44:52 -0500

    added print() function to separate_chaining.rb

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -77,4 +77,28 @@ class SeparateChaining
 
     @items = new_items
   end
+
+
+  def print
+    current_load_factor = @item_count / self.size.to_f
+    puts "Current load factor is #{current_load_factor}"
+    @items.inspect
+  end
+
 end
+
+
+def test_function
+  # require "./separate_chaining/separate_chaining.rb"
+
+  star_wars_movies = SeparateChaining.new(6)
+  star_wars_movies["Star Wars: The Phantom Menace"] = "Number One"
+  star_wars_movies["Star Wars: Attack of the Clones"] = "Number Two"
+  star_wars_movies["Star Wars: Revenge of the Sith"] = "Number Three"
+  star_wars_movies["Star Wars: A New Hope"] = "Number Four"
+  star_wars_movies["Star Wars: The Empire Strikes Back"] = "Number Five"
+  star_wars_movies["Star Wars: Return of the Jedi"] = "Number Six"
+  star_wars_movies.print()
+end
+
+# test_function()
