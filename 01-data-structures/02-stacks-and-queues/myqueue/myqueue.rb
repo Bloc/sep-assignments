1bf75b7b582d5fab20ebf46adccd5348e4324d42
Sam Dieck <samuel.dieck@gmail.com>
2016-12-07 17:06:14 -0800

    Assignment 2 completed

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -3,16 +3,22 @@ class MyQueue
   attr_accessor :tail
 
   def initialize
-    @queue = Array.new
+    @queue = []
     @head = @queue[0]
+    @tail = @queue.last
   end
 
   def enqueue(element)
+    @queue.unshift(element)
+    @head = @queue.first
+    @tail = @queue.last
   end
 
   def dequeue
+    @queue.delete_at(0)
   end
 
   def empty?
+    @queue.length == 0 ? true : false
   end
 end
