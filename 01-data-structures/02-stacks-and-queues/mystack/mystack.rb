d3050b471d4601488a4414cc95bfb75ce52855b3
JD Gonzales <jd_gonzales@icloud.com>
2016-12-17 22:40:48 -0600

    Pop method not complete on MyStack

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,28 @@ class MyStack
   end
 
   def push(item)
+     @top = item 
+     @stack[(@stack.length)] = @top
+      
   end
 
   def pop
+
+      temp = @stack[(@stack.length - 1)]
+
+      @stack[(@stack.length - 1)] = nil
+      
+      @top = @stack[(@stack.length - 1)]
+      
+      return temp
+      
   end
 
   def empty?
+      if @stack.length == 0
+          return true
+      else
+          return false
+      end
   end
 end
