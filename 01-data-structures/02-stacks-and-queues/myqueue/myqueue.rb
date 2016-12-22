57855dcc948843ab8ffee520ff26b51396f2aed0
Luther <lutheroto@gmail.com>
2016-12-22 00:30:48 -0500

    Implement Queue.

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,23 @@ class MyQueue
   end
 
   def enqueue(element)
+    if empty?
+      @tail = element
+    end
+    @head = element
+    @queue.insert(0, element)
   end
 
   def dequeue
+    result = tail
+    @queue.delete_at(-1)
+    @tail = @queue[-1]
+    if empty?
+      @head = nil
+    end
   end
 
   def empty?
+    @queue.empty?
   end
 end
