3d1fd29f5577f3b8e7ed0b9f81a5826ffc52ec52
Luther <lutheroto@gmail.com>
2016-12-26 21:23:05 -0500

    Implement HashClass.

--- 01-data-structures/04-hashes-part-1/hash_item.rb
+++ 01-data-structures/04-hashes-part-1/hash_item.rb
@@ -3,5 +3,11 @@ class HashItem
   attr_accessor :value
 
   def initialize(key, value)
+    @key = key
+    @value = value
+  end
+
+  def inspect
+    "#{@key.inspect} => #{@value.inspect}"
   end
 end
