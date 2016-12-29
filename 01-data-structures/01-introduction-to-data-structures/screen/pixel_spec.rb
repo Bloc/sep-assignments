c494ae0f7f796f786686a8700fa6357bd8c4f21e
Timothy Barnes <barnes7td@gmail.com>
2016-12-29 00:39:05 -0500

    Fixed code style and improve and harden specs

--- 01-data-structures/01-introduction-to-data-structures/screen/pixel_spec.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/pixel_spec.rb
@@ -1,25 +1,45 @@
-include RSpec
+require_relative 'pixel'
 
 RSpec.describe Pixel, type: Class do
-  let(:pixel) { Pixel.new(255, 255, 255, 0, 0) }
-
   describe "#initialize" do
     it "creates a pixel with the specified parameters" do
+      pixel = Pixel.new(255, 200, 160, 5, 7)
+       
       expect(pixel.red).to eq 255
+      expect(pixel.green).to eq 200
+      expect(pixel.blue).to eq 160
+      expect(pixel.x).to eq 5
+      expect(pixel.y).to eq 7
+    end
+    
+    it "corrects a red value if it's less than 0" do
+      pixel = Pixel.new(-7, 100, 100, 5, 5)
+      expect(pixel.red).to eq 0
+    end
+    
+    it "corrects a blue value if it's less than 0" do
+      pixel = Pixel.new(100, -10, 100, 5, 5)
+      expect(pixel.green).to eq 0
+    end
+    
+    it "corrects a green value if it's less than 0" do
+      pixel = Pixel.new(100, 100, -12, 5, 5)
+      expect(pixel.blue).to eq 0
+    end
+
+    it "corrects a red value if it's greater than 255" do
+      pixel = Pixel.new(256, 100, 100, 5, 5)
+      expect(pixel.red).to eq 255
+    end
+    
+    it "corrects a green value if it's greater than 255" do
+      pixel = Pixel.new(100, 258, 100, 5, 5)
       expect(pixel.green).to eq 255
+    end
+    
+    it "corrects a blue value if it's greater than 255" do
+      pixel = Pixel.new(100, 100, 300, 5, 5)
       expect(pixel.blue).to eq 255
-      expect(pixel.x).to eq 0
-      expect(pixel.y).to eq 0
-    end
-
-    it "corrects a pixel color value if it's less than 0" do
-      p = Pixel.new(-1, 0, 0, 0, 0)
-      expect(p.red).to eq 0
-    end
-
-    it "corrects a pixel color value if it's greater than 255" do
-      p = Pixel.new(256, 0, 0, 0, 0)
-      expect(p.red).to eq 255
     end
   end
 end
