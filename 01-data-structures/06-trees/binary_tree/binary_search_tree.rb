e221c94facdded450774548885a2178181df09fb
Luther <lutheroto@gmail.com>
2017-01-04 03:45:59 -0500

    Implement BinarySearchTree.

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -3,19 +3,115 @@ require_relative 'node'
 class BinarySearchTree
 
   def initialize(root)
+    @root = root
   end
 
   def insert(root, node)
+    if node < root
+      if root.left
+        insert(root.left, node)
+      else
+        root.left = node
+      end
+    else
+      if root.right
+        insert(root.right, node)
+      else
+        root.right = node
+      end
+    end
   end
 
   # Recursive Depth First Search
   def find(root, data)
+    if root
+      if root.title == data
+        root
+      else
+        find(root.left, data) || find(root.right, data)
+      end
+    end
   end
 
+  # Does not check to see if root is nil.
   def delete(root, data)
+    if root == @root && root.title == data
+      # Delete the root node.
+      if !root.left
+        @root = root.right
+        # Go left and find the right-most node from there.
+      elsif !root.left.right
+        root.left.right = root.right
+        @root = root.left
+      else
+        leaf_parent = root.left
+        while leaf_parent.right.right
+          leaf_parent = leaf_parent.right
+        end
+        leaf = leaf_parent.right
+        leaf_parent.right = leaf.left
+        leaf.left = root.left
+        leaf.right = root.right
+        @root = leaf
+      end
+      # See if root is the parent of the target node.
+    elsif root.left
+      if root.left.title != data
+        delete(root.left, data)
+        # Delete root.left.
+      elsif !root.left.left
+        root.left = root.left.right
+        # Go left and find the right-most node from there.
+      elsif !root.left.left.right
+        root.left.left.right = root.left.right
+        root.left = root.left.left
+      else
+        leaf_parent = root.left.left
+        while leaf_parent.right.right
+          leaf_parent = leaf_parent.right
+        end
+        leaf = leaf_parent.right
+        leaf_parent.right = leaf.left
+        leaf.left = root.left.left
+        leaf.right = root.left.right
+        root.left = leaf
+      end
+    elsif root.right
+      if root.right.title != data
+        delete(root.right, data)
+        # Delete root.right.
+      elsif !root.right.left
+        root.right = root.right.right
+        # Go left and find the right-most node from there.
+      elsif !root.right.left.right
+        root.right.left.right = root.right.right
+        root.right = root.right.left
+      else
+        leaf_parent = root.right.left
+        while leaf_parent.right.right
+          leaf_parent = leaf_parent.right
+        end
+        leaf = leaf_parent.right
+        leaf_parent.right = leaf.left
+        leaf.left = root.right.left
+        leaf.right = root.right.right
+        root.right = leaf
+      end
+    end
   end
 
   # Recursive Breadth First Search
-  def printf(children=nil)
+  def printf(children=[@root])
+    if !children.empty?
+      node = children.delete_at(0)
+      puts(node)
+      if node.left
+        children.push(node.left)
+      end
+      if node.right
+        children.push(node.right)
+      end
+      printf(children)
+    end
   end
 end
--- 01-data-structures/06-trees/binary_tree/node.rb
+++ 01-data-structures/06-trees/binary_tree/node.rb
@@ -1,9 +1,21 @@
 class Node
-  attr_accessor :title
-  attr_accessor :rating
+  include Comparable
+
+  attr_reader :title
+  attr_reader :rating
   attr_accessor :left
   attr_accessor :right
 
   def initialize(title, rating)
+    @title = title
+    @rating = rating
+  end
+
+  def <=> other
+    self.rating <=> other.rating
+  end
+
+  def to_s
+    "#@title: #@rating"
   end
 end
