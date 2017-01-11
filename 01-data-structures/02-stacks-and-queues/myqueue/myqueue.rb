425232e2d0375fa55b1805ff8d3036a5d2853d42
ianroberts131 <ianroberts131@gmail.com>
2017-01-11 04:43:34 +0000

    Complete Data Structures Assignment 2 - Stacks & Queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,4 +1,6 @@
 class MyQueue
+  # head representes the 'back' of the queue
+  # tail represents the 'front' of the queue
   attr_accessor :head
   attr_accessor :tail
 
@@ -8,11 +10,20 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue.push(element)
+    # Add element, so they go to the 'back' of the queue (i.e. the head)
+    @head = element
+    @tail = @queue[0]
   end
 
   def dequeue
+    removed_item = @queue.shift
+    # The 'front' has been removed (i.e. the tail). Need to reset tail.
+    @tail = @queue[0]
+    return removed_item
   end
 
   def empty?
+    @queue.length == 0
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,20 @@ class MyStack
   end
 
   def push(item)
+    @stack.push(item)
+    # Since LIFO, new items will be first to be removed. Thus, added item is the 'top'
+    self.top = item
   end
 
   def pop
+    # Grab the last element of the stack (i.e. the top)
+    popped_item = @stack.pop
+    # Since the top was removed, need to reset the top attribute
+    self.top = @stack.last
+    return popped_item
   end
 
   def empty?
+    @stack.length == 0
   end
 end
