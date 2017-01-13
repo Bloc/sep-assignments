e1ec1174d08d7bfe335812f7aa9f9b5bd9e5c843
Sahil Mehta <samehta91@gmail.com>
2017-01-13 11:50:00 -0800

    Introduction to data structures

--- 01-data-structures/01-introduction-to-data-structures/screen/pixel.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/pixel.rb
@@ -12,11 +12,23 @@ class Pixel
 
 
   def initialize(red, green, blue, x, y)
+    self.red = validate_color(red)
+    self.green = validate_color(green)
+    self.blue = validate_color(blue)
+    self.x = x
+    self.y = y
   end
 
   private
 
   def validate_color(color)
+    if color < 0
+      return 0
+    elsif color > 255
+      return 255
+    else
+      return color
+    end
   end
 
 end
--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,28 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @width = width
+    @height = height
+    @matrix = [width, [height, nil]]
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    @matrix[x][y] = pixel
   end
 
   def at(x, y)
+    if inbounds(x, y)
+      @matrix[x][y]
+    else
+      return nil
+    end
   end
 
   private
 
   def inbounds(x, y)
+    ((x < width || x >= 0) && (y < height || y >= 0))
   end
 
 end
