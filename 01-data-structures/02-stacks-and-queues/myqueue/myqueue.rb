a5c634861378d6b553b4bf027c49922912f27d6e
Steeve Postemus <spostemus.gmail.com>
2017-05-11 19:36:41 -0700

    finished stack and queue assignment

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,25 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def enqueue(element)
+    @queue.insert(0, element)
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def dequeue
+    @queue.delete_at(-1)
+    @tail = @queue[-1]
   end
 
   def empty?
+    if @queue.length == 0
+      true
+    else
+      false
+    end
   end
 end
