29c03a3a71c018a6cd2d1dfa07b1523228093e6f
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-05-22 13:42:46 -0400

    Stacks and queues complete

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -3,15 +3,26 @@ class MyStack
 
   def initialize
     @stack = Array.new
-    self.top = nil
+    self.top = @stack.last
   end
 
   def push(item)
+    @stack[@stack.length] = item
+    self.top = @stack.last
   end
 
   def pop
+    temp = @stack.last
+    @stack.delete(temp)
+    self.top = @stack.last
+    temp
   end
 
   def empty?
+    if @stack.length == 0
+      true
+    else
+      false
+    end
   end
 end
