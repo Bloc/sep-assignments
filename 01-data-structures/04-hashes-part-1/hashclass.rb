e54ff6b0ab1b3d4be0a222971ab92f0b85f736cb
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-05-23 17:52:03 -0400

    Basic hash functionality

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,23 +5,42 @@ class HashClass
   end
 
   def []=(key, value)
+    @items[key.sum % @items.size] = value
   end
 
 
   def [](key)
+    @items[key.sum % @items.size]
   end
 
   def resize
+    # for i in 0..@items.size
+    #   @items.push
+    # end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.size
   end
 
+  def add(v)
+    k = v.sum % @k
+    @items[k] = v
+  end
 end
+
+h = HashClass.new(6)
+h.[]=("Charlie", "Man")
+h.[]=("Twigg", "Dog")
+puts h.[]("Charlie")
+puts h.[]("Twigg")
+
+puts h.inspect
