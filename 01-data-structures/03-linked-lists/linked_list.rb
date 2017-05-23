53968aa24914aa0206d8e1913177140625515a79
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-05-23 00:11:34 -0400

    add_to_tail and print functioning

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -6,14 +6,30 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    if @head
+      i = @head
+      while i.next
+        i = i.next
+      end
+      i.next = node
+    else
+      @head = node
+    end
+    @tail = node
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+
   end
 
   # This method prints out a representation of the list.
   def print
+    i = @head
+    while i
+      puts i.data
+      i = i.next
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
@@ -28,3 +44,9 @@ class LinkedList
   def remove_front
   end
 end
+
+n1 = Node.new("Tom")
+ll = LinkedList.new
+ll.add_to_tail(n1)
+
+ll.print
