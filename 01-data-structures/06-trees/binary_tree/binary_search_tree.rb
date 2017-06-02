881e75955a43e58ea3a9c3664559859fa31f9603
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-06-02 12:53:10 -0400

    insert method implimented

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -3,13 +3,27 @@ require_relative 'node'
 class BinarySearchTree
 
   def initialize(root)
+    @root = root
   end
 
-  def insert(root, node)
+  def root
+    @root
+  end
+
+  def insert(root = @root, node)
+    parent = root
+    while true
+      if node.rating < parent.rating
+        parent.left ? parent = parent.left : (parent.left = node; return)
+      else
+        parent.right ? parent = parent.right : (parent.right = node; return)
+      end
+    end
   end
 
   # Recursive Depth First Search
-  def find(root, data)
+  def find(root = @root, data)
+    puts "Here" if root.title == data
   end
 
   def delete(root, data)
