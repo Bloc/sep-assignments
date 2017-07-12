524a10c915653b288b00f36bbd3b567e93b546a8
Dave Lively <davelively14@gmail.com>
2017-07-11 21:20:25 -0400

    Passes tests

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,34 @@ class Line
   end
 
   def join(person)
+    @members.push(person)
   end
 
   def leave(person)
+    @members.delete_if {|stored_person| stored_person == person}
   end
 
   def front
+    @members.first
   end
 
   def middle
+    @members[@members.length/2]
   end
 
   def back
+    @members.last
   end
 
   def search(person)
+    i = index(person)
+    i ? @members[i] : nil
   end
 
   private
 
   def index(person)
+    @members.index(person)
   end
 
 end
