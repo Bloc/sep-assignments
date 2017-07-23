96c4605e58455fd70974416f2f8785cfdf5849bd
Jigesh Parekh <JParekh117@gmail.com>
2017-07-22 23:00:30 -0700

    Stacks and Queues

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -1,5 +1,7 @@
 class MyStack
-  attr_accessor :top
+  attr_accessor :top, :stack
+
+  #LIFO Stack
 
   def initialize
     @stack = Array.new
@@ -7,11 +9,21 @@ class MyStack
   end
 
   def push(item)
+    stackIndex = stack.length
+    stack[stackIndex] = item #last position becomes items place
+    @top = stack.last #set top to last pos in stack array
   end
 
   def pop
+    popItem = @top
+    if !empty? 
+      stack.delete_at(stack.length - 1) 
+      @top = stack.last #set top to the new last position after pop deletion
+    end
+    return popItem
   end
 
   def empty?
+    stack.length == 0
   end
 end
