5f5cecda2937df0d7e34ba0647c00adb2695e8ec
Robert Pirritano <rpirritano@gmail.com>
2017-08-17 14:02:30 -0700

    working on scree data structure problem

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,24 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    self.width = width
+    self.height = height
+    self.matrix = Array.new(width, Array.new(height, nil))
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    self.matrix[x][y] = pixel
   end
 
   def at(x, y)
+    inbounds(x,y) ? self.matrix[x][y] : nil
   end
 
   private
 
   def inbounds(x, y)
+    ((x < width || x > 0) || y < height || y > 0)
   end
 
 end
