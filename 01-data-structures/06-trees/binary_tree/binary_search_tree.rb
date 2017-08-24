710e88202a1a77d0c701a778e66c1ff19e9a13a7
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-08-24 01:34:31 +0000

    cleaned up binary tree code

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -11,35 +11,21 @@ class BinarySearchTree
   end
 
   def insert(root, node)
-    if root == nil
-      self.root = node
-      return
-    end
-    
-    #node1 = find(root, node.title)
-    #if node1
-    #  parent = node1.parent
-    #  delete_aux(root, node1)
-    #  if parent == nil
-    #    parent = self.root
-    #  end
-    #  insert(parent, node)
-    #  return
-    #end
-    
+
     if node.rating < root.rating
       if root.left
         insert(root.left, node)
       else
         root.set_left(node)
       end
-    else
-      if root.right
-        insert(root.right, node)
-      else
-        root.set_right(node)
-      end
+      return
     end
+    if root.right
+      insert(root.right, node)
+    else
+      root.set_right(node)
+    end
+    return
   end
 
   # Recursive Depth First Search
@@ -47,6 +33,7 @@ class BinarySearchTree
     if root == nil
       return nil
     end
+    
     if root.title == data
       return root
     end
@@ -54,9 +41,6 @@ class BinarySearchTree
   end
   
   def leftmost(root)
-    if root == nil
-      return nil
-    end
     if root.left == nil
       return root
     end
@@ -64,9 +48,6 @@ class BinarySearchTree
   end
   
   def rightmost(root)
-    if root == nil
-      return nil
-    end
     if root.right == nil
       return root
     end
@@ -74,19 +55,23 @@ class BinarySearchTree
   end
   
   def replace_node(root, node)
-    node && node.detach
-    if node && node != root.left
-      node.set_left(root.left) # the new node replaces the parent, so it has the same value of left
-    end
-    if node && node != root.right
-      node.set_right(root.right)
-    end
+
     if root.parent && root.parent.left == root
       root.parent.set_left(node)
     end
     if root.parent && root.parent.right == root
       root.parent.set_right(node)
     end
+    
+    return unless node
+    
+    if node != root.left
+      node.set_left(root.left) # the new node replaces the parent, so it has the same value of left
+    end
+    if node != root.right
+      node.set_right(root.right)
+    end
+    
     if root == self.root
       self.root = node
     end
@@ -106,8 +91,11 @@ class BinarySearchTree
     # the right of everything on the left. If the deleted node
     # has no children, then the replacement is nil
     if !node.left && !node.right # if no children, can just remove from parent's children
-      newnode = nil
-    elsif node.right # then we can let the replacement be the leftmost node on the right
+      replace_node(node, nil)
+      return node
+    end
+    
+    if node.right # then we can let the replacement be the leftmost node on the right
       newnode = leftmost(node.right)
     else # node.right == nil && node.left != nil  Then the new node is the rightmost node on the left
       newnode = rightmost(node.left)
