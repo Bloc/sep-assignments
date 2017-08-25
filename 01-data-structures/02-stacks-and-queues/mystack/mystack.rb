713cdabe46f6b3f4cbce3333d9ae944548c0cf9d
rrsg123 <rrsg123@gmail.com>
2017-08-25 11:16:51 -0600

    finish section 2

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -6,12 +6,20 @@ class MyStack
     self.top = nil
   end
 
+  # Item added to top of array with unshift, top moves to new index[0].
   def push(item)
+    @stack.unshift(item)
+    self.top = @stack[0]
   end
 
+  # Marks new top as one under original, removes original.
   def pop
+    self.top = @stack[1]
+    @stack.shift
   end
 
+  # If stack is empty, comes back as true
   def empty?
+    @stack.length == 0
   end
 end
