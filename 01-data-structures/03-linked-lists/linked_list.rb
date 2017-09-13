d2adf2930bbb6432d9f78497eb35b1eaaafb5d2e
Tyrant Davis <davistyrant@gmail.com>
2017-09-13 09:28:29 -0500

    Implements #delete.

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -43,6 +43,17 @@ class LinkedList
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if @head == node
+      remove_front
+    elsif @tail == node
+      remove_tail
+    else
+      current = @head
+      until current.next == node
+        current = current.next
+      end
+      current.next = node.next
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
