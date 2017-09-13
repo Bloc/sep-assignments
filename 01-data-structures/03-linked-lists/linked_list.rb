d882bff26c37e5e4d8b291026264e63cd0ed3ba2
Tyrant Davis <davistyrant@gmail.com>
2017-09-13 09:23:16 -0500

    Implements #add_to_tail.

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -8,9 +8,17 @@ class LinkedList
     @head = nil
     @tail = nil
   end
-  
+
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    current = @head
+    if @tail.nil?
+      @tail = node
+    elsif @tail
+      temp = @tail
+      temp.next = node
+      @tail = node
+      @tail.next = nil
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
