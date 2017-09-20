ba0946186a01ee3ccdf3843d92d4a8bc750114a2
Tyrant Davis <davistyrant@gmail.com>
2017-09-20 12:05:09 -0500

    Implements #insert.

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -4,10 +4,15 @@ class BinarySearchTree
 
   def initialize(root)
     self.root = root
-    
+
   end
 
   def insert(root, node)
+    if root.rating < node.rating
+      (root.right.nil?) ? root.right = node : insert(root.right, node)
+    else
+      (root.left.nil?) ? root.left = node : insert(root.left, node)
+    end
   end
 
   # Recursive Depth First Search
