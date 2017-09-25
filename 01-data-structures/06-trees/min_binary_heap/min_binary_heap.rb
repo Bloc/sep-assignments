accc7c598a0dc4426f17f5455f51454f097d427d
Tyrant Davis <davistyrant@gmail.com>
2017-09-25 15:39:49 -0500

    Implements #find, #delete, #printf

--- 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
+++ 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
@@ -25,5 +25,43 @@ class MinBinaryHeap
       puts "Ooops. There was an error. Please ensure there are no duplicates and retry."
     end
   end
+  
+  # Recursive Depth First Search
+  def find(root, data)
+    return nil if  data.nil?
+
+    if root.title.eql?(data)
+      return root
+    elsif !root.left.nil?
+      find(root.left, data)
+    elsif !root.right.nil?
+      find(root.right, data)
+    end
+  end
+
+  def delete(root, data)
+    return nil if data.nil?
+    remove = find(root, data)
+    remove.title, remove.rating = nil, nil
+  end
+
+  # Recursive Breadth First Search
+  def printf(children=nil)
+    queue = [root] #FIFO
+    children = []
+
+    until queue.empty?
+      sub_root = queue.slice!(0)
+
+      queue << sub_root.left if !sub_root.left.nil?
+      queue << sub_root.right if !sub_root.right.nil?
+
+      children << "#{sub_root.title : sub_root.rating}"
+    end
+
+    children.each do |child|
+      puts child
+    end
+  end
 
 end
