8bb4e2fc31e485202756fb66c5277bf2e37019f8
Tyrant Davis <davistyrant@gmail.com>
2017-09-25 15:18:23 -0500

    Implements #insert.

--- 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
+++ 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
@@ -2,9 +2,28 @@ require_relative 'node'
 
 class MinBinaryHeap
   attr_accessor :root
-  
+
   def initialize
     slef.root = root
+  end
+
+  def insert(root, node)
+    if root.rating > node.rating
+      current_root = root
+      root = node
+      node = current_root
+      insert(root, node)
+    elsif root.left.nil?
+      root.left = node
+    elsif !root.left.nil? && root.right.nil?
+      root.right = node
+    elsif (!root.left.nil? && !root.right.nil? ) && (!root.left.left.nil? && !root.left.right.nil?)
+      insert(root.right, node)
+    elsif !root.left.nil? && !root.right.nil?
+      insert(root.left, node)
+    else
+      puts "Ooops. There was an error. Please ensure there are no duplicates and retry."
+    end
   end
 
 end
