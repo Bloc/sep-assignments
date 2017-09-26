469b548b8f9abc68b1611c04034a375d643cb3eb
Jigesh Parekh <JParekh117@gmail.com>
2017-09-25 23:21:23 -0700

    update before os install

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -3,16 +3,46 @@ require_relative 'node'
 class BinarySearchTree
 
   def initialize(root)
+    #the first node
+    @root = root
   end
 
   def insert(root, node)
+    #check if the new inserted node has a higher age than the existing root entry
+    if node.rating > root.rating
+      #node age is higher than root age - therefore insert it to the right
+      if root.right == nil
+        root.right = node
+      else
+        insert(root.right, node)
+      end
+    else
+      #node age is lower than root age - insert it to the left of the root
+      if root.left == nil
+        root.left = node
+      else
+        insert(root.left, node)
+      end
+    end
   end
+
 
   # Recursive Depth First Search
   def find(root, data)
+    node = root
+    if root != nil 
+      if root.title != data #recurisve statement searches down left and right until object is found
+        node = find(root.right, data)
+        if node.nil?
+          node = find(root.right, data)
+        end
+      end
+    end
+    return node
   end
 
   def delete(root, data)
+    
   end
 
   # Recursive Breadth First Search
