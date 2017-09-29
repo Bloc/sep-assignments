4a1c9a3a6f8a70f884042f84dbb6220a7e9b1eac
Tyrant Davis <davistyrant@gmail.com>
2017-09-29 12:56:32 -0500

    Modifies seperate_chaining.rb #initialize.

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -1,33 +1,43 @@
+require_relative 'node'
 require_relative 'linked_list'
 
 class SeparateChaining
   attr_reader :max_load_factor
 
   def initialize(size)
+    @items = Array.new(size)
     @max_load_factor = 0.7
   end
 
   def []=(key, value)
+
   end
 
   def [](key)
+
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+
   end
 
   # Calculate the current load factor
   def load_factor
+
   end
 
   # Simple method to return the number of items in the hash
   def size
+
   end
 
   # Resize the hash
   def resize
+
   end
+
+
 end
