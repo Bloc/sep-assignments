6cc44f0aef5e1e1758451ae1d5f5ac91e31f2340
Tyrant Davis <davistyrant@gmail.com>
2017-09-29 12:49:55 -0500

    Updates #add_to_tail, #remove_tail, #delete, #add_to_front, #remove_front, #print. Adds #size.

--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -4,61 +4,46 @@ class LinkedList
   attr_accessor :head
   attr_accessor :tail
 
-  def initialize
+  def initialize()
     @head = nil
     @tail = nil
   end
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
-    current = @head
-    if @tail.nil?
+    if @head.nil?
+      @head = node
+      @tail = @head
+    else
+      @tail.next = node
       @tail = node
-    elsif @tail
-      temp = @tail
-      temp.next = node
-      @tail = node
-      @tail.next = nil
     end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
-    current = @head
-    if @tail.nil?
-      return nil
-    elsif @head.next.nil?
+    if @head === @tail
+      @head = nil
       @tail = nil
     else
-      until current.next == @tail
-        current = current.next
+      node = @head
+      until ((node.nil?) && (node.next.eql?(@tail))) do
+        node = node.next
       end
-      current.next = nil
-      @tail = current
-    end
-  end
-
-  # This method prints out a representation of the list.
-  def print
-    current = @head
-
-    puts current.data
-
-    until current.next == nil
-      current = current.next
-      puts current.data
+      @tail = node
+      @tail.next = nil
     end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
-    if @head == node
+    if @head.eql?(node)
       remove_front
     elsif @tail == node
       remove_tail
     else
       current = @head
-      until current.next == node
+      until current.next.eql?(node)
         current = current.next
       end
       current.next = node.next
@@ -67,21 +52,51 @@ class LinkedList
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
-    if ((@head == nil) && (@tail == nil))
-      @head = node
-      @head.next = nil
+    node.next = @head
+    @head = node
+
+    if @tail.nil?
       @tail = node
-      @tail.next = nil
-    else
-      current = @head
-      @head = node
-      @head.next = current
     end
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
-    @tail == @head ? @head = nil : @head = @head.next
+    node = @head
+    if @head.eql?(@tail)
+      @head = nil
+      @tail = nil
+    elsif !@head.nil?
+      @head = node.next
+
+      if @head.next.nil?
+        @tail = @head
+      end
+    end
+    return node
+  end
+
+  def size
+    i = 0
+    if !@head.nil?
+      i = 1
+      j = @head
+      while !j.eql?(@tail)
+        j = j.next
+        i += 1
+      end
+    end
+    return i
+  end
+
+  # This method prints out a representation of the list.
+  def print
+    node = @head
+
+    until node.eql?(nil) do
+      puts node.data
+      node = node.next
+    end
   end
 
 end
