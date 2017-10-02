482f9aa24a12c4c1ae3896aa2bbdd582ab1750f3
Tyrant Davis <davistyrant@gmail.com>
2017-10-02 14:30:02 -0500

    WIP... Updates binary tree insertion.

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -56,45 +56,45 @@ class BinarySearchTree
 
 end
 
-debate = Node.new("The Great Debaters", 79)
-swat = Node.new("S.W.A.T.", 48)
-equalizer = Node.new("The Equalizer", 60)
-out = Node.new("Get Out", 99)
-ride1 = Node.new("Ride Along", 19)
-ride2 = Node.new("Ride Along 2", 14)
-think = Node.new("Think Like A Man", 79)
-think2 = Node.new("Think Like A Man Too", 53)
-love = Node.new("Love And Basketball", 82)
-last = Node.new("Transformers: The Last Knight", 15)
-moon = Node.new("Transformers: Dark Of The Moon", 35)
-trans = Node.new("Transformers", 57)
-
-btree = BinarySearchTree.new(debate)
-
-btree.insert(debate, swat)
-btree.insert(debate, equalizer)
-btree.insert(debate, out)
-btree.insert(debate, ride1)
-btree.insert(debate, ride2)
-btree.insert(debate, think)
-btree.insert(debate, think2)
-btree.insert(debate, love)
-btree.insert(debate, last)
-btree.insert(debate, moon)
-btree.insert(debate, trans)
-
-btree.printf
-
-count = 0
-numbers = []
-
-until count > 10000
-  numbers << Node.new(count, count)
-  count += 1
-end
-
-numbers_tree  = BinarySearchTree.new(numbers.first)
-
-numbers.each { |num| numbers_tree.insert(numbers.first, count) }
-
-puts Benmark.measure { numbers_tree.find(numbers.first, 5000) }
+# debate = Node.new("The Great Debaters", 79)
+# swat = Node.new("S.W.A.T.", 48)
+# equalizer = Node.new("The Equalizer", 60)
+# out = Node.new("Get Out", 99)
+# ride1 = Node.new("Ride Along", 19)
+# ride2 = Node.new("Ride Along 2", 14)
+# think = Node.new("Think Like A Man", 79)
+# think2 = Node.new("Think Like A Man Too", 53)
+# love = Node.new("Love And Basketball", 82)
+# last = Node.new("Transformers: The Last Knight", 15)
+# moon = Node.new("Transformers: Dark Of The Moon", 35)
+# trans = Node.new("Transformers", 57)
+#
+# btree = BinarySearchTree.new(debate)
+#
+# btree.insert(debate, swat)
+# btree.insert(debate, equalizer)
+# btree.insert(debate, out)
+# btree.insert(debate, ride1)
+# btree.insert(debate, ride2)
+# btree.insert(debate, think)
+# btree.insert(debate, think2)
+# btree.insert(debate, love)
+# btree.insert(debate, last)
+# btree.insert(debate, moon)
+# btree.insert(debate, trans)
+#
+# btree.printf
+#
+# count = 0
+# numbers = []
+#
+# until count > 10000
+#   numbers << Node.new(count, count)
+#   count += 1
+# end
+#
+# numbers_tree  = BinarySearchTree.new(numbers.first)
+#
+# numbers.each { |num| numbers_tree.insert(numbers.first, count) }
+#
+# puts Benmark.measure { numbers_tree.find(numbers.first, 5000) }
