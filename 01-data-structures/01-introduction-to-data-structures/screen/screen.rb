a9a8b6367b0ac3d60bb0fa5e370fd9f09facf2dc
Tyler D. Franklin <irishlep84@gmail.com>
2017-10-03 16:34:22 -0500

    Checkpoint 1 complete

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -1,4 +1,11 @@
 require_relative 'pixel'
+require 'matrix'
+
+class Matrix  
+  def []=(i, j, x)
+    @rows[i][j] = x
+  end
+end
 
 class Screen
   attr_accessor :width
@@ -6,18 +13,24 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @width = width
+    @height = height
+    @matrix = Matrix.zero(height, width)
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    @matrix[x, y] = pixel
   end
 
   def at(x, y)
+    inbounds(x,y)
   end
 
   private
 
   def inbounds(x, y)
+    @matrix[x,y] == 0 ? nil : @matrix[x,y]
   end
 
 end
