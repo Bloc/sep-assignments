7ad4c68c4810976cf81f37c451db14908457ad50
ScottL <sjlteacher@gmail.com>
2017-10-05 15:21:05 -0400

    Some progress on hashes, but problem with resize and assigning values.

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,23 +5,38 @@ class HashClass
   end
 
   def []=(key, value)
+    i = index(key, size)
+    resize if @items[i] != nil && @items[i] != value
+    @items[i] = value
   end
 
 
   def [](key)
+    @items[index(key, size)]
   end
 
   def resize
+    hash_primes = [0, 5, 11, 19, 37, 67, 131, 283]
+    size = [hash_primes.find_index(size) + 1]
+    temp_items = @items
+    @items = Array.new(size)
+    temp_items.each_with_index do |key, value|
+      @items[index(key, size)] = value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    x = 0
+    key.each_codepoint { |c| x += c }
+    x % size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
 end
