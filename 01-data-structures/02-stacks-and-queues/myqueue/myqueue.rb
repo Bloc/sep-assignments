aa0b77bef5e45a8099f13a19e1fce7bb0fdd0563
robotscissors <cjbazin@gmail.com>
2017-10-16 21:43:44 -0700

    completed stacks and queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,6 +1,7 @@
 class MyQueue
   attr_accessor :head
   attr_accessor :tail
+  attr_accessor :queue
 
   def initialize
     @queue = Array.new
@@ -8,11 +9,18 @@ class MyQueue
   end
 
   def enqueue(element)
+    self.tail = element
+    self.head = element if queue.length === 0
+    queue[queue.length] = element
   end
 
   def dequeue
+    queue.slice!(0,1)
+    self.head = queue[0]
   end
 
   def empty?
+    return true unless self.head
+    false
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -1,5 +1,6 @@
 class MyStack
   attr_accessor :top
+  attr_accessor :stack
 
   def initialize
     @stack = Array.new
@@ -7,11 +8,19 @@ class MyStack
   end
 
   def push(item)
+    self.top = item
+    stack[stack.length] = item
   end
 
   def pop
+    top_element = self.top
+    stack.slice!(-1)
+    self.top = stack[stack.length-1]
+    top_element
   end
 
   def empty?
+    return true unless self.top
+    false
   end
 end
