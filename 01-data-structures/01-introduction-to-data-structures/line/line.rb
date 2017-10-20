bb2d326930023eacabf110ce2f30c5163712de86
Joe <jdg006@lvc.edu>
2017-10-20 21:21:50 +0000

    assignment 1 complete

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,58 @@ class Line
   end
 
   def join(person)
+    members.push(person)
   end
 
   def leave(person)
+    i = 0
+    while i<members.length
+      if members[i] == person
+        members.delete_at(i)
+        return
+      else
+        i+=1
+      end
+    end
+    return "Person Not Found"
   end
 
   def front
+    members[0]
   end
 
   def middle
+    members[(members.length/2).floor]
   end
 
   def back
+    members[-1]
   end
 
   def search(person)
+    i = 0
+    while i<members.length
+      if members[i] == person
+        return members[i]
+      else
+        i+=1 
+      end
+    end
+    return 
   end
 
   private
 
   def index(person)
+    i = 0
+    while i<members.length
+      if members[i] == person
+        return i
+      else
+        i+=1 
+      end
+    end
+    return "Person Not Found"
   end
 
 end
