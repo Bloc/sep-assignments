a4d972e272086edafe47695f0a56872d48bffd79
Tyrant Davis <davistyrant@gmail.com>
2017-10-26 10:57:07 -0500

    WIP... Several additions made.

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -37,53 +37,53 @@ class BinarySearchTree
 
   # Recursive Breadth First Search
   def printf(children=nil)
-    queue = [@root] #FIFO
+    queue = [@root]
     children = []
 
-    until queue.empty?
-      sub_root = queue.slice!(0)
+    while queue.size > 0
+      sub_root = queue.shift
+      if sub_root.left != nil
+        queue.push(sub_root.left)
+      end
 
-      queue << sub_root.left if !sub_root.left.nil?
-      queue << sub_root.right if !sub_root.right.nil?
-
-      children << "#{sub_root.title} : #{sub_root.rating}"
+      if sub_root.right != nil
+        queue.push(sub_root.right)
+      end
+      children.push("#{sub_root.title} : #{sub_root.rating}")
     end
-
-    children.each do |child|
-      puts child
-    end
+    children.each { |out| puts out }
   end
 
 end
 
-# debate = Node.new("The Great Debaters", 79)
-# swat = Node.new("S.W.A.T.", 48)
-# equalizer = Node.new("The Equalizer", 60)
-# out = Node.new("Get Out", 99)
-# ride1 = Node.new("Ride Along", 19)
-# ride2 = Node.new("Ride Along 2", 14)
-# think = Node.new("Think Like A Man", 79)
-# think2 = Node.new("Think Like A Man Too", 53)
-# love = Node.new("Love And Basketball", 82)
-# last = Node.new("Transformers: The Last Knight", 15)
-# moon = Node.new("Transformers: Dark Of The Moon", 35)
-# trans = Node.new("Transformers", 57)
-#
-# btree = BinarySearchTree.new(debate)
-#
-# btree.insert(debate, swat)
-# btree.insert(debate, equalizer)
-# btree.insert(debate, out)
-# btree.insert(debate, ride1)
-# btree.insert(debate, ride2)
-# btree.insert(debate, think)
-# btree.insert(debate, think2)
-# btree.insert(debate, love)
-# btree.insert(debate, last)
-# btree.insert(debate, moon)
-# btree.insert(debate, trans)
-#
-# btree.printf
+debate = Node.new("The Great Debaters", 79)
+swat = Node.new("S.W.A.T.", 48)
+equalizer = Node.new("The Equalizer", 60)
+out = Node.new("Get Out", 99)
+ride1 = Node.new("Ride Along", 19)
+ride2 = Node.new("Ride Along 2", 14)
+think = Node.new("Think Like A Man", 79)
+think2 = Node.new("Think Like A Man Too", 53)
+love = Node.new("Love And Basketball", 82)
+last = Node.new("Transformers: The Last Knight", 15)
+moon = Node.new("Transformers: Dark Of The Moon", 35)
+trans = Node.new("Transformers", 57)
+
+btree = BinarySearchTree.new(debate)
+
+btree.insert(debate, swat)
+btree.insert(debate, equalizer)
+btree.insert(debate, out)
+btree.insert(debate, ride1)
+btree.insert(debate, ride2)
+btree.insert(debate, think)
+btree.insert(debate, think2)
+btree.insert(debate, love)
+btree.insert(debate, last)
+btree.insert(debate, moon)
+btree.insert(debate, trans)
+
+btree.printf
 #
 # count = 0
 # numbers = []
