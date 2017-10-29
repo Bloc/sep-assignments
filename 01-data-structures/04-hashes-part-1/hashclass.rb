799dd8577bea5bcff33951a8765621b31accb15e
robotscissors <cjbazin@gmail.com>
2017-10-29 11:43:16 -0700

    cleaned up code hashclass passed

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -4,23 +4,19 @@ class HashClass
     @items = Array.new(size)
   end
 
-  def []=(key, value) #The Custom Define Operator
+  def []=(key, value) #Setter - The Custom Define Operator
     new_item = HashItem.new(key, value) #Create a new Item to attach to the Hash
     hash_index = index(key, size) #Create the index for our new item
-
     if @items[hash_index] == nil
       # If the value at hash index is nil (or empty)
-      # Assign the item to the index
       @items[index(key,size)] = new_item
     elsif @items[hash_index].key != key
       # Else if the key of index does not equal key of the new item
       # The Hash needs to be resized
-      # So we resize it and then pair the key and value
-      while !@items[index(key,size)].nil? do
+      while !@items[index(key,size)].nil? do #resize until there is space
         self.resize
       end
       @items[index(key,size)] = new_item
-
     elsif @items[hash_index].key == key && @items[hash_index].value != value
       # Else if there is a collision and the values are different
       # Just Resize the class Array
@@ -28,16 +24,13 @@ class HashClass
       @items[index(key,size)] = new_item
     end
   end
-  
-  def [](key)
-    key_of = @items[index(key,size)].key
-    value_of = @items[index(key,size)].value
-    value_of
+
+  def [](key) #Getter
+    return @items[index(key,size)].value
   end
 
   def resize
     new_size = 0
-    temp_items = []
     temp_items = @items
     new_size = @items.length * 2
     @items = Array.new(new_size)
