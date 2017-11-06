e821cb5a85483cb6664741005ea69decc2af4368
Tyrant Davis <davistyrant@gmail.com>
2017-11-06 11:26:05 -0600

    Completes benchmark.

--- 01-data-structures/06-trees/benchmark.rb
+++ 01-data-structures/06-trees/benchmark.rb
@@ -3,49 +3,49 @@ require_relative 'binary_tree/binary_search_tree'
 require_relative 'min_binary_heap/min_binary_heap'
 
 
-tree_root = Node.new("tree root", 42)
-heap_root = Node.new("heap root", 42)
-tree = BinarySearchTree.new(tree_root)
-heap = MinBinaryHeap.new(heap_root)
+btree_root = Node.new("tree root", 24)
+min_heap_root = Node.new("heap root", 24)
+tree = BinarySearchTree.new(btree_root)
+heap = MinBinaryHeap.new(min_heap_root)
 
 
 
 
-n = 100000
+nums = 100000
 
-Benchmark.bm(100) do |x|
-  x.report("tree insert") do
-    for i in 1..n do
-      new_node = Node.new("tree", Random.rand(100000))
-      tree.insert(tree_root, new_node)
+Benchmark.bm(25) do |x|
+  x.report("tree #insert") do
+    for i in 1..nums do
+      new_node = Node.new("tree", Random.rand(nums))
+      tree.insert(btree_root, new_node)
     end
   end
-   x.report("heap insert") do
-     for i in 1..n do
-       new_node = Node.new("heap", Random.rand(100000))
-       heap.insert(heap_root, new_node)
+   x.report("heap #insert") do
+     for i in 1..nums do
+       new_node = Node.new("heap", Random.rand(nums))
+       heap.insert(min_heap_root, new_node)
      end
    end
 end
 
 target = Node.new("target", 50000)
-tree.insert(tree_root, target)
-heap.insert(heap_root, target)
+tree.insert(btree_root, target)
+heap.insert(min_heap_root, target)
 
-Benchmark.bm(100) do |x|
-  x.report("tree find") do
-      tree.find(tree_root, 50000)
+Benchmark.bm(25) do |x|
+  x.report("tree #find") do
+      tree.find(btree_root, 50000)
   end
-   x.report("heap find") do
-     heap.find(heap_root, 50000)
+   x.report("heap #find") do
+     heap.find(min_heap_root, 50000)
    end
 end
 
-Benchmark.bm(100) do |x|
-  x.report("tree delete") do
-      tree.delete(tree_root, 50000)
+Benchmark.bm(25) do |x|
+  x.report("tree #delete") do
+      tree.delete(btree_root, 50000)
   end
-   x.report("heap delete") do
-     heap.delete(heap_root, 50000)
+   x.report("heap #delete") do
+     heap.delete(min_heap_root, 50000)
    end
 end
