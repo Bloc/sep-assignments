c81f7e5a2cd47c96c08a7d8c1e23041621014ca7
Tyrant Davis <davistyrant@gmail.com>
2017-11-06 11:25:12 -0600

    Modifies min_binary_heap.rb

--- 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
+++ 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
@@ -17,25 +17,25 @@ def insert(root, node)
   else
     if root.left.nil?
       root.left = node
-    elsif root.right.nil? && root.left != nil
+    elsif root.right.nil? && !root.left.nil?
       root.right = node
-    elsif root.left != nil && root.right != nil && root.left.left != nil && root.left.right != nil
+    elsif !root.left.nil? && !root.right.nil? && !root.left.left.nil? && !root.left.right.nil?
       insert(root.right, node)
-    elsif root.left != nil && root.right != nil
+    elsif !root.left .nil? && !root.right.nil?
       insert(root.left, node)
     end
   end
 end
 
   def find(root, data)
-    if root.nil? || data.nil?
+    if data.nil?
       return nil
     else
-      if root.title == data
+      if root.title.eql?(data)
         return root
-      elsif root.left != nil
+      elsif !root.left.nil?
         find(root.left, data)
-      elsif root.right != nil
+      elsif !root.right.nil?
         find(root.right, data)
       end
     end
@@ -45,24 +45,53 @@ end
     if  data.nil?
       return nil
     else
-      target_node = find(root, data)
-      target_node.nil? ? nil : (target_node.title = nil && target_node.rating = nil)
+      target = find(root, data)
+      target.nil? ? nil : (target.title.nil? && target.rating.nil?)
     end
   end
 
   def printf
-    queue = [@root]
+    q = [@root]
     output = []
-    while queue.length > 0
-      new_root = queue.shift
-      if new_root.left != nil
-        queue.push(new_root.left)
+    while q.length > 0
+      new_root = q.shift
+      if !new_root.left.nil?
+        q.push(new_root.left)
       end
-      if new_root.right != nil
-        queue.push(new_root.right)
+      if !new_root.right.nil?
+        q.push(new_root.right)
       end
       output.push("#{new_root.title}: #{new_root.rating}")
     end
-    output.each {|x| puts x}
+    output.each {|x| print x + "\s"}
   end
 end
+
+ray = Node.new("Ray", 81)
+ali = Node.new("Ali", 67)
+unbreakable = Node.new("Unbreakable", 68)
+american = Node.new("American Gangster", 80)
+kong = Node.new("Kong: Skull Island", 76)
+think = Node.new("Think Like A Man", 53)
+mib = Node.new("Men In Black", 93)
+debaters = Node.new("The Great Debaters", 79)
+reloaded = Node.new("The Matrix Reloaded", 73)
+matrix = Node.new("The Matrix", 87)
+wood = Node.new("The Wood", 62)
+revolt = Node.new("The Matrix Revolutions", 36)
+
+binaryHeap = MinBinaryHeap.new(ray)
+
+binaryHeap.insert(ray, ali)
+binaryHeap.insert(ray, unbreakable)
+binaryHeap.insert(ray, american)
+binaryHeap.insert(ray, kong)
+binaryHeap.insert(ray, think)
+binaryHeap.insert(ray, mib)
+binaryHeap.insert(ray, debaters)
+binaryHeap.insert(ray, reloaded)
+binaryHeap.insert(ray, matrix)
+binaryHeap.insert(ray, wood)
+binaryHeap.insert(ray, revolt)
+
+binaryHeap.printf
