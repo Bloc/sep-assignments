5102f7937744fc14a7925ed43c7e02c0ac90943a
robotscissors <cjbazin@gmail.com>
2017-11-27 20:35:51 -0800

    Fix issues with fib(0) and fib(1)

--- algorithms-introduction/fibonacci_iterative.rb
+++ algorithms-introduction/fibonacci_iterative.rb
@@ -1,11 +1,13 @@
 def fib(n)
   # Add functionality here
+  return 0 if n === 0
+  return 1 if n === 1
   # SET fib_0 to 0
   fib_0 = 0
   # SET fib_1 to 1
   fib_1 = 1
   # FOR each value from 0 to one less than n
-  (0..n-1).each do
+  (1..n-1).each do
   #   SET temp to fib_0
     temp = fib_0
   #   ASSIGN fib_1 to fib_0
@@ -27,3 +29,14 @@ puts fib(6)
 puts fib(7)
 puts fib(8)
 puts fib(9)
+puts fib(10)
+puts fib(11)
+puts fib(12)
+puts fib(13)
+puts fib(14)
+puts fib(15)
+puts fib(16)
+puts fib(17)
+puts fib(18)
+puts fib(19)
+puts fib(20)
