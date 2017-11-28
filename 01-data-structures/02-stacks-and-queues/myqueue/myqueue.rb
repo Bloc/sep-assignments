cfcdb1f801907feb12983832ab047cdf9ae33308
rtrunkhill <rtrunkhill@gmail.com>
2017-11-28 14:41:54 -0800

    just need to figure out rspec

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,18 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue += [(element)]
+    @head = @queue[0]
+    @tail = @queue.last
   end
 
   def dequeue
+    @queue.shift
+    @head = @queue[0]
+    @tail = @queue.last
   end
 
   def empty?
+    @queue.empty? ? (return true) : (return false)
   end
 end
