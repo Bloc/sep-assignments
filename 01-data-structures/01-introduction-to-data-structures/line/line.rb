05b19d974732c5e16cd964b1739b2a3a50293ab5
Steven Peniche <stevenmpeniche@gmail.com>
2017-12-29 10:34:42 -0500

    Line of People: complete

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,37 @@ class Line
   end
 
   def join(person)
+		self.members.push person
   end
 
   def leave(person)
+		self.members.delete person
   end
 
   def front
+		self.members[0]
   end
 
   def middle
+		self.members[(self.members.length / 2).floor]
   end
 
   def back
+		self.members[-1]
   end
 
   def search(person)
+		if self.members.include?(person)
+			return person
+		else
+			return nil
+		end
   end
 
   private
 
   def index(person)
+		self.members.index(person)
   end
 
 end
