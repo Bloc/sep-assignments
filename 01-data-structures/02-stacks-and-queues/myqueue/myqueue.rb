73fe9ce3578d8105325c7ab606dfe0fd0323e3db
Constance Gobert <constancegobert@hotmail.fr>
2018-01-28 17:19:45 +0100

    asssignment completed

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,20 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue.last
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element
+    @head = @queue[0]
+    @tail = @queue.last
   end
 
   def dequeue
+    @queue.shift
   end
 
   def empty?
+    @queue.length == 0 ? true : false
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,18 @@ class MyStack
   end
 
   def push(item)
+    @stack[@stack.length] = item
+    self.top = @stack.last
   end
 
   def pop
+    temp = self.top
+    @stack = @stack - [self.top]
+    self.top = @stack.last
+    temp
   end
 
   def empty?
+    @stack.length == 0 ? true : false
   end
 end
