16495be6d2fe22b649ee3041a155b4a5a1379dba
Jessica-Koch <jessicakoch136@icloud.com>
2018-02-02 22:04:10 -0800

    finish stack

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,23 @@ class MyQueue
   end
 
   def enqueue(element)
+    index = @queue.length
+
+    @queue << element
   end
 
   def dequeue
+    @queue.delete_at(0)
+
+    @head = @queue[0]
+    puts @queue
   end
 
   def empty?
+    if @queue.length < 1
+      true
+    else
+      false
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,25 @@ class MyStack
   end
 
   def push(item)
+    @top = @stack.length - 1
+    @stack << item
+    @top = @stack[@stack.length - 1]
   end
 
+
   def pop
+    raise "Stack is empty" if self.empty?
+    lastIndex = @stack.length - 1
+    temp = @stack.delete_at(lastIndex)
+    @top = @stack[lastIndex - 1]
+    temp
   end
 
   def empty?
+    if @stack.length <= 0
+      true
+    else
+      false
+    end
   end
 end
