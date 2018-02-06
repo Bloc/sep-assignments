0952325e8476cc992a3e978d3ec4b0fed1c27596
Camille <camilleseguette@gmail.com>
2018-02-05 19:32:53 -0800

    Intro to Data Structures Assignment - completed

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,44 @@ class Line
   end
 
   def join(person)
+    # members[members.size() > 0 ? x.size() -1 : 0] = 6
+    self.members[members.size() > 0 ? -1 : 0] = person
   end
 
   def leave(person)
+    self.members.delete(person)
   end
 
   def front
+    self.members[members.first()]
   end
 
   def middle
+    self.members[members.slice(3)]
   end
 
   def back
+    self.members[members.last()]
   end
 
   def search(person)
+    # for loop, check each thing in list, compare with person
+    for i in self.members
+      if i == person
+        person
+      end
+    end
   end
 
   private
 
   def index(person)
+    if search(person)
+      # find the index of person
+      self.members.index(person)
+    else
+      # otherwise nil else statement
+      nil
+    end
   end
-
 end
