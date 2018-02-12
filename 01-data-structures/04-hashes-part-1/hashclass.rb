35ce46689c4b8ee2a858d60da0ab410d231f47ee
Jessica-Koch <jessicakoch136@icloud.com>
2018-02-11 22:09:12 -0600

    add benchmark data

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,28 +1,34 @@
 class HashClass
 
   def initialize(size)
-    @items = Array.new(size)
+    items = Array.new(size)
+    @items = items
   end
 
   def []=(key, value)
-    @items.map.with_index{|i|}
+    @items.push([key, value])
+    @items.delete_if{|k, v| k == nil}
 
-    end
-    @items = {key: value}
+    @items
   end
 
 
   def [](key)
-    key
+    @items.assoc(key)[1]
   end
 
   def resize
+    len = @items.length
+    arr = Array.new(len, [])
+
+    @items = @items + arr
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    @items
   end
 
   # Simple method to return the number of items in the hash
