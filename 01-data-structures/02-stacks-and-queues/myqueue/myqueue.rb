1dc0c85c48c4771c140635be3877175ebf2a7037
Jocelyn Hsu <hsu.joce@gmail.com>
2018-03-17 14:39:31 -0700

    Complete Stacks and Queue assignment

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,20 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue << element
+    @head = @queue[0]
+    @tail = @queue.last
   end
 
   def dequeue
+    item = @queue[0]
+    @queue.delete(item)
+    @head = @queue[0]
+    @tail = @queue.last
+    return item
   end
 
   def empty?
+    @queue.length == 0
   end
 end
