23b00534ebac868c8837f0eff0fa6c558967fff4
Natalie Wyll <nataliewyll@yahoo.com>
2018-03-18 14:03:36 -0500

    refactoring

--- 02-algorithms-and-complexity/02-searching/fibonacci_recursive.rb
+++ 02-algorithms-and-complexity/02-searching/fibonacci_recursive.rb
@@ -1,9 +1,6 @@
 def fib_recursive(n)
-  if (n == 0)
-    return 0
-  elsif (n == 1)
-    return 1
-  else
-    return fib_recursive(n-1) + fib_recursive(n-2)
-  end
+  return 0 if n == 0
+  return 1 if n == 1
+
+  return fib_recursive(n-1) + fib_recursive(n-2)
 end
