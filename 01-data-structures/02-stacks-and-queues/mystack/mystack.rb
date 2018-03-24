7f891dbc6f1d1b2684afb80c3cd5c85482a4d20e
Thomas Conkwright <tmconkwright@gmail.com>
2018-03-24 17:19:16 -0400

    Stack and Queues Completed

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,26 @@ class MyStack
   end
 
   def push(item)
+    @stack[@stack.length] = item
+    self.top = @stack[@stack.length - 1]
   end
 
   def pop
+    if @stack.empty?
+      nil
+    else
+      last = @stack[@stack.length - 1]
+      @stack.delete_at(@stack.length - 1)
+      self.top = @stack[@stack.length - 1]
+      return last
+    end
   end
 
   def empty?
+    if @stack.length > 0
+      false
+    else
+      true
+    end
   end
 end
