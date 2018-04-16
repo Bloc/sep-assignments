f4678cd39a5222c84630589761caf6d3228b2b0f
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-15 19:13:32 -0700

    working on stack

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,18 @@ class MyStack
   end
 
   def push(item)
+    @stack.insert(0,item)
   end
 
   def pop
+    @stack.shift
   end
 
   def empty?
+    @stack.empty?
+  end
+
+  def top
+    @stack.first
   end
 end
