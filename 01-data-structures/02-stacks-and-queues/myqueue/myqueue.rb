ae86136bf509705c795c3f78c4c954108671eeec
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-16 08:42:19 -0700

    updated queue to avoid using pop,push,empty commands

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,23 +8,27 @@ class MyQueue
     @tail = @queue[-1]
   end
 
+#  do not use existing push command,keep pointers for head/tail
   def enqueue(element)
-    @queue.push(element)
+    @queue << element
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
+# do not use existing pop command, keep pointers for head/tail
   def dequeue
-    @queue.pop
+    @tail = @queue.slice(-1)
+    @queue = @queue.slice!(0..-2)
+    @head = @queue[0]
+    @tail
   end
 
+ # avoid using empty?
   def empty?
-    @queue.empty?
-  end
-
-  def head
-    @queue.first
-  end
-
-  def tail
-    @queue.last
+    if @queue.length == 0
+      return true
+    else
+      return false
+    end
   end
 end
