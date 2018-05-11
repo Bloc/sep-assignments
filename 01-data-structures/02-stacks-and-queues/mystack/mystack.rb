f1d4df33c73a263cd3291afc5bb88348b5638011
Edward Choi <edwardchoi.2011@gmail.com>
2018-05-11 10:10:49 -0700

    stacks and queues

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -3,15 +3,29 @@ class MyStack
 
   def initialize
     @stack = Array.new
-    self.top = nil
+    @top = nil
+    @counter = -1
   end
 
   def push(item)
+    @counter += 1
+    @top = item
+    @stack[@counter] = @top
   end
 
   def pop
+    temp = @top
+    @stack[@counter] = nil
+    @counter -= 1
+    @top = @stack[@counter]
+    return temp
   end
 
   def empty?
+    if @counter < 0 
+      return true
+    else
+      return false
+    end
   end
 end
