6a0d2e71f84515db608085df7f38327c733ad036
ShangMing Cherng <night1300013@gmail.com>
2018-05-26 22:54:17 -0700

    finish screen using 2 dimension array

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,13 +6,22 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    self.matrix = Array.new(width){Array.new(height){ nil }}
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    pixel.x = x
+    pixel.y = y
+    matrix[x][y] = pixel
   end
 
   def at(x, y)
+    if x > 0 && y > 0
+      matrix[x][y]
+    else
+      nil
+    end
   end
 
   private
