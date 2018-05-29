0fa11792f9a809be6162baa9b73c3999c313850f
Jessica Roque <jroque.dev@gmail.com>
2018-05-28 20:21:09 -0700

    line and screen data structures

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -2,33 +2,45 @@
 # There is a front to the line, as well as a back.
 # People may leave the line whenever they see fit and those behind them take their place.
 class Line
-  attr_accessor :members
+  attr_reader :members
 
   def initialize
     self.members = []
   end
 
   def join(person)
+	self.members << person
   end
 
   def leave(person)
+	self.members.delete(person)
   end
 
   def front
+	self.members.first
   end
 
   def middle
+	if (self.members.length % 2 == 0)
+		median = self.members.length / 2
+	else
+		median = (self.members.length + 1) / 2
+	end
+	self.members[median]
   end
 
   def back
+	self.members.last
   end
 
   def search(person)
+	self.members.bsearch {|i| i == person}
   end
 
   private
 
   def index(person)
+	
   end
 
 end
--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -1,18 +1,29 @@
 require_relative 'pixel'
 
 class Screen
-  attr_accessor :width
-  attr_accessor :height
-  attr_accessor :matrix
+  attr_reader :width
+  attr_reader :height
+  attr_reader :matrix
 
   def initialize(width, height)
+	self.width = width
+	self.height = height
+
+	row = generate_row(width)
+	self.matrix = Array.new(height, row)
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+	self.matrix[y[x]] = pixel
   end
 
   def at(x, y)
+	if ((x || y) < 0)
+		return nil
+	end
+
+	self.matrix[y[x]]
   end
 
   private
@@ -20,4 +31,13 @@ class Screen
   def inbounds(x, y)
   end
 
+  def generate_row(width)
+		i = 0
+		rowItems = []
+		while i < width
+			rowItems << [nil]
+			i += 1
+		end
+		rowItems
+  end
 end
