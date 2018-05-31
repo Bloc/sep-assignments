30c8304e242ee9aac432b065b5491937fff7c928
ShangMing Cherng <night1300013@gmail.com>
2018-05-30 22:21:28 -0700

    finish separate chaining

--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -6,25 +6,104 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    #if there is tail, add node after tail
+    if self.tail
+      self.tail.next = node
+    end
+    #if the linked_list is empty
+    if self.head.nil?
+      self.head = node
+    end
+    #set the tail to the node
+      self.tail = node
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    #the case that there is only one linked_list
+    if self.head == self.tail
+      self.head = nil
+      self.tail = nil
+    end
+
+    temp_node = self.head
+    while temp_node
+      if temp_node.next == self.tail
+        temp_node.next = nil
+        self.tail = temp_node
+      end
+      temp_node = temp_node.next
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    temp_node = self.head
+    while temp_node
+      puts "Key:#{temp_node.key}, Value:#{temp_node.value}"
+      temp_node = temp_node.next
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if self.head == node && self.head == self.tail
+      self.head = nil
+      self.tail = nil
+      return
+    elsif self.head == node
+      self.head = node.next
+      node.next = nil
+      return
+    end
+
+    temp_node = self.head
+    while temp_node
+      if temp_node.next == node && temp_node.next == self.tail
+        temp_node.next = nil
+        self.tail = temp_node
+        return
+      elsif temp_node.next == node
+        temp_node.next = temp_node.next.next
+        node.next = nil
+        return
+      end
+      temp_node = temp_node.next
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    if self.head
+      node.next = self.head
+    else
+      self.tail = node
+    end
+      self.head = node
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    if self.head == self.tail
+      self.head = nil
+      self.tail = nil
+    else
+      temp_node = self.head
+      self.head = self.head.next
+      temp_node.next = nil
+      temp_node
+    end
+  end
+
+  def find_value(key)
+    temp_node = self.head
+    while temp_node
+      if temp_node.key == key
+        return temp_node.value
+      end
+      temp_node = temp_node.next
+    end
+
+    nil
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -5,29 +5,55 @@ class SeparateChaining
 
   def initialize(size)
     @max_load_factor = 0.7
+    @items = Array.new(size)
+    @entries = 0.0
   end
 
   def []=(key, value)
+    @entries += 1
+    self.resize if load_factor > @max_load_factor
+    hash_index = self.index(key,size)
+    #if the array is nil, create linked_list 
+    @items[hash_index] = @items[hash_index] || LinkedList.new
+    @items[hash_index].add_to_tail(Node.new(key, value))
+
   end
 
   def [](key)
+    @items[index(key,size)].find_value(key)
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Calculate the current load factor
   def load_factor
+    @entries/size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
   # Resize the hash
   def resize
+    temp_items = @items
+    @items = Array.new(2 * size)
+    temp_items.each do |llist|
+      if !llist.nil?
+        temp_node = llist.head
+        while temp_node
+          hash_index = self.index(temp_node.key,size)
+          @items[hash_index] = LinkedList.new
+          @items[hash_index].add_to_tail(Node.new(temp_node.key, temp_node.value))
+          temp_node = temp_node.next
+        end
+      end
+    end
   end
 end
