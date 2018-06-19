b28fe455c719e14baefc865a35d9eb7f2e85af62
John <jpkarvonen@gmail.com>
2018-06-19 19:53:14 -0400

    create placeholders/complete heap spec

---
+++ 01-data-structures/06-trees/min_binary_heap/min_heap_tree.rb
@@ -0,0 +1,24 @@
+require_relative 'node'
+
+class MinHeapTree
+  attr_accessor :root
+
+  def initialize(root)
+    @root = root
+  end
+
+  def insert(root, node)
+  end
+
+  # Recursive Depth First Search
+  def find(root, data)
+  end
+
+  def delete(root, data)
+  end
+
+  # Recursive Breadth First Search
+  def printf(root)
+  end
+
+end
