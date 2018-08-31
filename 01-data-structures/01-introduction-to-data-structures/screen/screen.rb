c9e643cacdf23402009a95c18a49d2676affc0c5
Lassiter Gregg <lassitergregg@gmail.com>
2018-08-31 17:46:41 -0500

    rspec tests passing for line and screen

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -1,23 +1,32 @@
 require_relative 'pixel'
+require 'matrix'
 
 class Screen
   attr_accessor :width
   attr_accessor :height
   attr_accessor :matrix
 
-  def initialize(width, height)
+  def initialize(width = 0, height = 0)
+    self.width = width
+    self.height = height
+
+    @matrix = Array.new
+    (0..height).each do |j|
+      @matrix[j] = Array.new
+      (0..width).each do |i|
+        @matrix[j][i] = nil
+      end
+    end
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    return nil if x > self.width || y > self.height
+    @matrix[x][y] = pixel
   end
 
   def at(x, y)
-  end
-
-  private
-
-  def inbounds(x, y)
+    return @matrix[x][y]
   end
 
 end
