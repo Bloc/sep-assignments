33a5153e0a4f3b32395f713c17cf6d822a1165df
Lisa Caton <lisacaton01@gmail.com>
2018-12-05 10:32:24 -0500

    both examples complete

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,62 @@ class MyStack
   end
 
   def push(item)
+    #attempt #1
+    # self.top ||=""
+    # self.top << item
+    #fails 2nd it context
+
+    #attempt #2
+    #add the item at the 1st index (self.top)
+    #self.top = ""
+    #self.top.insert(-1, item)
+    #WORKS!
+
+    #attempt #2
+    #add the item at the 1st index (self.top)
+    self.top = item
+    @stack << item
+
   end
 
-  def pop
+  def pop()
+    #attempt #1
+    # if (@stack.length === 0)
+    #    nil
+    # else
+    #    @stack.delete(-1)
+    # end
+
+    #attempt #2
+    #puts self.top.delete(-1).to_s unless self.top.index[-1] === 0
+    #puts self.top.index[-1].to_s unless self.top.index[-1] === 0
+
+    #attempt #3
+    last_person = @stack.delete_at(-1)
+    self.top = @stack[-1]
+    return last_person
+
+
   end
 
   def empty?
+    #attempt #1
+    # [].empty? ? true : false
+    #fails 2nd it context
+
+    #attempt #1
+    # self.top = []
+    # self.top.empty? ? true : false
+    #fails 2nd it context
+
+    #attempt #3
+    # self.top ||=""
+    # self.top.empty? ? true : false
+    #WORKS!
+
+    #attempt #4
+    @stack.length == 0
+    #WORKS!
+
   end
 end
