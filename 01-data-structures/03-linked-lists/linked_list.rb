e0f7f01fa77fc7024b9152a0f75117ac600a7469
Lisa Caton <lisacaton01@gmail.com>
2018-12-11 14:04:01 -0500

    linked list complete

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -4,27 +4,89 @@ class LinkedList
   attr_accessor :head
   attr_accessor :tail
 
+  def intitialize
+    self.head = nil
+    self.tail = nil
+  end
+
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    if self.head === nil
+      self.head = node
+      self.tail = self.head
+      self.tail.next = nil
+    else 
+      #beg. the traversal
+      current = self.head
+      while current.next != nil
+        #as long as it's not the tail continue
+        current = current.next
+      end
+      current.next = node
+      self.tail = current.next
+      self.tail.next = nil
+    end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    if self.tail.nil?
+      return nil
+    elsif self.head.next.nil?
+      self.tail = nil
+    else
+      current_node = self.head
+      until current_node.next == self.tail
+        current_node = current_node.next
+        #Technical Coach
+        #continue to run this line `current_node = current_node.next'
+        #until 'until current_node.next == self.tail` results in true
+        #ME
+        #until the "current_node.next == self.tail" results in true
+        #the pointer will continue to be used as the current node's pointer
+      end
+      current_node.next = nil
+      self.tail = current_node
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    current = self.head
+    puts current.data
+    while current.next != nil
+      current = current.next
+      puts current.data
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if self.head == node
+      remove_front
+    elsif self.tail == node
+      remove_tail
+    else
+      current_node = self.head
+      until current_node.next == node
+        current_node = current_node.next
+      end
+      current_node.next = node.next
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    node.next = self.head
+    self.head.nil? && self.tail.nil? ? (self.head = node && self.tail = node) : self.head = node
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    if (self.head === nil) || (self.head.next === nil) || (self.head === self.tail)
+      self.head = nil
+    else
+      self.head = self.head.next
+    end
   end
 end
--- 01-data-structures/03-linked-lists/node.rb
+++ 01-data-structures/03-linked-lists/node.rb
@@ -3,5 +3,19 @@ class Node
   attr_accessor :data
 
   def initialize(data)
+     self.data = data
+     self.next = nil
   end
 end
+
+#Each element on the linked list is represented by a node that contains 
+#two attributes:
+
+  #1. The value that the element holds.
+  #2. A pointer to the next node in the list.
+
+  #[ value | next ] -> [ value | next ] -> [ value | next ] -> X (nil)
+
+#To get an element from a singly linked list you have to traverse it from
+#head to tail. That’s the only direction possible because nodes on singly
+#linked lists don’t have back pointers.
