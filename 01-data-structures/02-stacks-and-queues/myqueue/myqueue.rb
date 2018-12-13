761cb91ca740eee66aacaee9ad74861440488e60
Lisa Caton <lisacaton01@gmail.com>
2018-12-13 13:42:18 -0500

    queue, removed comments

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -10,35 +10,6 @@ class MyQueue
   end
 
   def enqueue(element)
-
-    #attempt #1
-    # new_item = element
-    # @queue.insert(-1, element)
-    # @head = @queue[0]
-    # @tail = @queue[-1]
-    #WORKS
-
-    #attempt #2
-    # @queue ||=""
-    # @queue.push(element)
-    # @head = @queue[0]
-    # @tail = @queue[-1]
-    #WORKS
-    #Don't use PUSH in the method > via instructions
-
-    #attempt #3
-    #@queue << element
-    #@head = @queue[0]
-    #@tail = @queue[-1]
-
-#-----------------------------------
-    #attempt #4 - Mentor major help in guidance!
-      #first check if queue is empty
-      #if the array is empty:
-        #assign element to head and tail
-      #if not empty put the new element at the end of the queue
-        #the passed argument becomes both the first 
-          #& last item in the array
     if empty?
       self.queue[0] = element
       self.head = element
@@ -51,41 +22,6 @@ class MyQueue
   end
 
   def dequeue
-    #attempt #1
-    #@queue.empty? ? true : @queue.delete_at[0]
-    #fails 1st it context at: q.dequeue
-
-    #attempt #2
-    #@queue.length === 0 ? true : @queue.delete_at[0]
-    #fails 1st it context at: q.dequeue
-
-    #attempt #3
-    # if (@queue.length > 0)
-    #   return true
-    # else
-    #   return false
-    # end
-    #fails 1st it context at: q.dequeue
-    # @queue.length === 0 ? true :
-
-    #attempt #4
-    # first_person = @queue.delete_at(0)
-    # return first_person unless first_person.empty?
-    # @head = @queue[0]
-    # @tail = @queue[-1]
-
-    #attempt #5 - didn'tfinish thought - Time with mentor!
-    #use self over @ bc @ creates a local variable
-    #first_person = @queue.delete_at(0)
-    #return first_person unless first_person.empty?
-    #@head = @queue[0]
-    #@tail = @queue[-1]
-
-
-    #attempt #6
-    #remove item from the front
-    #reassign the head to index[0]
-    #reassign the tail to index[-1]
     self.queue.delete_at(0)
     self.head = self.queue[0]
     self.tail = self.queue[-1]
