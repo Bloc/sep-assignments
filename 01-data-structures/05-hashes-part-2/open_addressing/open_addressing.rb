30b923b7c79fa70ef908c29bf2236a3a779fda86
Lisa Caton <lisacaton01@gmail.com>
2018-12-20 12:04:49 -0500

    hashes-part-2, code complete

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -2,29 +2,72 @@ require_relative 'node'
 
 class OpenAddressing
   def initialize(size)
+    @items = Array.new(size)
+    @size = size
   end
 
   def []=(key, value)
+   unique_index = index(key, @size)#calling the method!
+   hashed_item = @items[unique_index]#created the Hash with the Unique Index!
+
+    if hashed_item.nil?
+    @items[unique_index] = Node.new(key, value)
+    elsif hashed_item.key != key
+      while @items[index(key, @size)].key != nil && @items[index(key, @size)].key != key
+        resize
+        re_index = index(key, @size)
+        break if @items[re_index] == nil
+      end
+      self[key] = value
+    elsif hashed_item.key == key && hashed_item.value != value
+      if next_open_index(unique_index) == -1
+        resize
+        re_index = index(key, @size)
+        @items[re_index].value = value
+      else
+        new_index = next_open_index(index(key, @size))
+        @items[new_index] = value
+      end
+    end
   end
 
   def [](key)
+    item = @items[index(key, size)]
+    item.nil? ? nil : item.value
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
+
   def index(key, size)
+    key.sum % size
   end
 
   # Given an index, find the next open index in @items
   def next_open_index(index)
+    until (@items[index].nil?) || (index > size - 1)
+      index += 1
+    end
+    index = -1 if index > size - 1
+    index
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @size
   end
 
   # Resize the hash
   def resize
+    @size = @size * 2
+    expanded_hash = Array.new(@size)
+
+    @items.each do |item|
+      if item != nil
+        expanded_hash[index(item.key, @size)] = item
+      end
+    end
+    @items = expanded_hash
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -4,27 +4,70 @@ class LinkedList
   attr_accessor :head
   attr_accessor :tail
 
+  def initialize
+    self.head = nil
+    self.tail = nil
+  end
+
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    return unless node
+    self.head ||= node
+    self.tail.next = node if self.tail
+    self.tail = node
+    self.tail.next = nil
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    if self.tail.nil?
+      return nil
+    elsif self.head.next.nil?
+      self.tail = nil
+    else
+      current_node = self.head
+      until current_node.next == self.tail
+        current_node = current_node.next
+      end
+      current_node.next = nil
+      self.tail = current_node
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    node = self.head
+    while node != self.tail
+      break if node.nil?
+      puts node.data
+      node = node.next
+    end
+    puts self.tail.data
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if self.head == node
+      remove_front
+    elsif self.tail == node
+      remove_tail
+    else
+      current_node = self.head
+      until current_node.next == node
+        current_node = current_node.next
+      end
+      current_node.next = node.next
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    node.next = self.head
+    self.head.nil? && self.tail.nil? ? (self.head = node && self.tail = node) : self.head = node
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    self.tail == self.head ? self.head = nil : self.head = self.head.next
   end
 end
