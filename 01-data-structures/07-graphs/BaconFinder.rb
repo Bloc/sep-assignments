3921c5e2d578b94b2dd76a6487cd06f4c4d523f2
Lisa Caton <lisacaton01@gmail.com>
2019-01-31 14:27:04 -0500

    updating baconador

--- 01-data-structures/07-graphs/BaconFinder.rb
+++ 01-data-structures/07-graphs/BaconFinder.rb
@@ -1,7 +1,11 @@
-include 'node'
+require_relative 'Node'
 
 class BaconFinder
   attr_accessor :visited
+
+  def initialize
+    @visited = Array.new
+  end
 
   def find_kevin_bacon(actor)
     films = []
@@ -22,35 +26,4 @@ class BaconFinder
     (films.length <= 6 && films.length > 0) ? (puts "Connected by films: #{films.all}") : (puts "Not connected by 6 or less films")
   end
 end
-
-#Function:
-#1. create a class called the BaconFinder
-#2. have the ruby method, attr_accessor - which will automatically set up getters and setters for the instance variable :visited
-#3. define the method 'find_kevin_bacon' with the parameter of actor
-#4. set an empty array to 'films'
-#5. in the method find_kevin_bacon, **add every parameter** that is passed through it, to the instance variable :visited
-#6. in line10, we see that each actor has a method attached to it called, film_actor_hash
-#7. on the object 'actor.film_actor_hash', start a loop with the block parameters: (1)title (2)actors (|title, actors|)
-#8. run a loop inside of the object 'actor.film_actor_hash', on the object 'actors.each', that will have a single block parameter |a|
-#9. actors.each |a| means the block parameter |a| allows the searching through every item in the previous block parameter | --, actors|
-#10. question = does the parameter block |a| equal 'Kevin Bacon' in any of these 'film_actor_hash's?
-#11. if yes, add that specific title to our empty array 'films'
-
-#('films' = only titles with 'Kevin Bacon' - present as one of the actors!)
-#('visited' = every single actor parameter passed to the method find_kevin_bacon!)
-
-#12. start an if loop, immediately check every single item in the 'visited' array.(which is every parameter we've passed in the method from the beginning'find_kevin_bacon')
-#13. if those names != 'Kevin Bacon'
-#14. run the recusion method again passing that name (that isn't 'Kevin Bacon') as the parameters, 
-#15. which will grab that specific title, then set the variable 'temp' to equal those values of those titles
-
-#16. add titles, (w/out 'Kevin Bacon'), to array 'films'
-#17. add temp to films - *Isn't ths the same as #16^?*
-
-#(we've adding the string titles (with name 'Kevin Bacon' as the actor) to the array 'films')
-#(we're adding the array of titles (that did not include 'Kevin Bacon' as the actor) to the array 'films')
-
-
-
-
 
